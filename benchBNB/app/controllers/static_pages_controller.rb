9c16428cb7924480a3c2515e60b122e442d19ff5
David Veytsman <dveyts@yahoo.com>
2017-10-20 17:31:09 -0400

    End of the day

--- benchBNB/app/controllers/static_pages_controller.rb
+++ benchBNB/app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def root
-    debugger
+  
   end
 end
---
+++ benchBNB/frontend/actions/session_actions.js
@@ -0,0 +1,19 @@
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+
+const receiveCurrentUser = (currentUser) => {
+
+};
+const receiveErrors = (errors) => {
+
+};
+export const login = (user) => {
+
+};
+export const logout = () => {
+
+};
+export const signup = (user) => {
+
+};
